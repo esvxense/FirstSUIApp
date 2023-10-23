ac25ca822c3baa80230a45152eb566f53b05ebb2
esvxense <esvxense@gmail.com>
2023-10-23 15:21:18 +0300

    Initial Commit

---
+++ FirstSUIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstSUIApp
+//
+//  Created by Alexander Snitko on 23.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstSUIApp/FirstSUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSUIAppApp.swift
+//  FirstSUIApp
+//
+//  Created by Alexander Snitko on 23.10.23.
+//
+
+import SwiftUI
+
+@main
+struct FirstSUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
